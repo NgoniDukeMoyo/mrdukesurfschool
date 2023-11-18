c740987775b7681c02661bd6fbb20c5d612ffd1c
NgoniDukeMoyo <dukemoyo7@gmail.com>
2023-11-18 17:29:54 +0000

    remove secret key risk

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ if 'USE_AWS' in os.enviro:
     #bucket config
     AWS_STORAGE_BUCKET_NAME = 'surfschoolobjects'
     AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
-    AWS_ACCESS_KEY_ID = 'AKIARJC22ZQCXSPPFAMW'
-    AWS_SECRET_ACCESS_KEY = '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'
+    AWS_ACCESS_KEY_ID = ''
+    AWS_SECRET_ACCESS_KEY = ''
     AWS_S3_CUSTOM_DOMAIN_NAME = f'{ AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
