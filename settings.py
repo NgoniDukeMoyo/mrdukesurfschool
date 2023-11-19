b6e42deb70efeb157b4b9a89e13078d4b7b53094
NgoniDukeMoyo <dukemoyo7@gmail.com>
2023-11-19 16:01:12 +0000

    django files

--- settings.py
+++ settings.py
@@ -1,7 +1,16 @@
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/mdeia/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 if 'USE_AWS' in os.enviro:
     #bucket config
-    AWS_STORAGE_BUCKET_NAME = 'surfschoolobjects'
+    AWS_STORAGE_BUCKET_NAME = 'mrdukesurfschoollatest'
     AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
-    AWS_ACCESS_KEY_ID = ''
-    AWS_SECRET_ACCESS_KEY = ''
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN_NAME = f'{ AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    #other
+    'storages'
